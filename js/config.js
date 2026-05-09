dfcab4b81d4fb493f48d3c2c617977fa0bad30df
zxymiku <zxymiku@outlook.com>
2026-05-10 01:52:08 +0800

    更新默认配置 URL 以指向新的 JSON 文件

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const DEFAULT_CONFIG_URL = './example.json';
+const DEFAULT_CONFIG_URL = 'https://git.zxymiku.top/https://github.com/zxymiku/kaoshi/releases/download/json/kaoshi.json';
 
 const STORAGE_KEYS = {
   config: 'kaoshi_config',
